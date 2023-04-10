928df81de5039d36e1ef06a662f77fc08e1d937b
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-04-11 01:46:28 +0900

    commit

--- BaekJoon#1918/main.swift
+++ BaekJoon#1918/main.swift
@@ -5,7 +5,61 @@
 //  Created by 김병엽 on 2023/04/07.
 //
 
-import Foundation
+func getPriority(ch: Character) -> Int{
+    switch ch {
+    case "+":
+        return 1
+    case "-":
+        return 1
+    case "*":
+        return 2
+    case "/":
+        return 2
+    case "(":
+        return 0
+    default:
+        break
+    }
+    return 0
+}
 
-print("Hello, World!")
+let input = Array(readLine()!)
+var ans = [Character]()
+var stack = [Character]()
 
+for ch in input {
+    if ch.isLetter {
+        ans.append(ch)
+    } else {
+        //(까지 다뽑기
+        if ch == "(" {
+            stack.append(ch)
+        } else if ch == ")" {
+            while true {
+                let popData = stack.removeLast()
+                if popData == "(" {break}
+                ans.append(popData)
+            }
+        } else if stack.isEmpty || getPriority(ch: stack.last!) < getPriority(ch: ch) {
+            //stack이 비어있거나, 지금 들어갈 우선순위가 더 크다면 push
+            stack.append(ch)
+        } else {
+            while !stack.isEmpty {
+                let top = stack.last!
+                if getPriority(ch: top) >= getPriority(ch: ch) {
+                    let popData = stack.removeLast()
+                    ans.append(popData)
+                } else {
+                    break
+                }
+            }
+            stack.append(ch)
+        }
+    }
+}
+
+while !stack.isEmpty {
+    ans.append(stack.removeLast())
+}
+
+print(String(ans))
